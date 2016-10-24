016628630b2f520c36389c4a5a877634a7ce38cd
andres <andres_herrero@hotmail.es>
2016-10-24 14:07:49 +0200

    data a data3

--- web/javascript/dsLinechart1.js
+++ web/javascript/dsLinechart1.js
@@ -1,6 +1,7 @@
 var 	formatAsPercentage = d3.format(".2s");
 var parseDate = d3.time.format("%d-%b-%y").parse;
-var data = [
+_data="data";
+/*var */data = [
         {date: "1-May-12", close: 58.13, close1: 45.58},
         {date: "30-Apr-12", close: 53.98, close1: 47.58},
         {date: "27-Apr-12", close: 67.00, close1: 51.58},
@@ -9,15 +10,24 @@ var data = [
         {date: "24-Apr-12", close: 130.48, close1: 96.58}
     ]
     ;
-var data2 = [
-        {date: "1-May-12", close: 48.13, close1: 25.58},
-        {date: "30-Apr-12", close: 43.98, close1: 47.58},
-        {date: "27-Apr-12", close: 57.00, close1: 61.58},
-        {date: "26-Apr-12", close: 79.70, close1: 85.58},
-        {date: "25-Apr-12", close: 89.00, close1: 93.58},
-        {date: "24-Apr-12", close: 83.48, close1: 106.58}
-    ]
-    ;
+//var data2 = [
+//        {date: "1-May-12", close: 48.13, close1: 25.58},
+//        {date: "30-Apr-12", close: 43.98, close1: 47.58},
+//        {date: "27-Apr-12", close: 57.00, close1: 61.58},
+//        {date: "26-Apr-12", close: 79.70, close1: 85.58},
+//        {date: "25-Apr-12", close: 89.00, close1: 93.58},
+//        {date: "24-Apr-12", close: 83.48, close1: 106.58}
+//    ]
+//    ;
+//var data3 = [
+//        {date: "1-May-12", close: 48.13, close1: 25.58},
+//        {date: "30-Apr-12", close: 43.98, close1: 47.58},
+//        {date: "27-Apr-12", close: 57.00, close1: 61.58},
+//        {date: "26-Apr-12", close: 79.70, close1: 85.58},
+//        {date: "25-Apr-12", close: 89.00, close1: 93.58},
+//        {date: "24-Apr-12", close: 83.48, close1: 106.58}
+//    ]
+//    ;
 function dsLineChart(){
 // Set the dimensions of the canvas / graph
     var margin = {top: 30, right: 20, bottom: 30, left: 50},
@@ -48,7 +58,7 @@ function dsLineChart(){
         .y(function(d) { return y(d.close1); });
 
 // Adds the svg canvas
-    var svg = d3.select("body")
+    /*var */svg = d3.select("body")
         .append("svg")
         .data([data])
         .attr("width", width + margin.left + margin.right)
@@ -75,7 +85,7 @@ function dsLineChart(){
 
     svg.append("path")
         //.data([data])
-        .attr("class", "line2")
+        .attr("class", "line2") //"line"
         .style("stroke", "red")
         .attr("d", valueline2);
         // Add the X Axis
@@ -90,9 +100,9 @@ function dsLineChart(){
             .call(yAxis);
 
     //});
-
-
+    changeData();
 }
+
 function removeChart(){
 //alert("asd");
     // Set the dimensions of the canvas / graph
@@ -111,7 +121,8 @@ function removeChart(){
     var yAxis = d3.svg.axis().scale(y)
         .orient("left").ticks(5);
 
-    data2.forEach(function(d) {
+    //data2.forEach(function(d) {
+    data.forEach(function(d) {
         d.date = parseDate(d.date);
         d.close = +d.close;
         d.close1 = +d.close1;
@@ -127,28 +138,54 @@ function removeChart(){
         .y(function(d) { return y(d.close1); });
 //
 //    // Scale the range of the data
-    x.domain(d3.extent(data2, function(d) { return d.date; }));
-    y.domain([0, d3.max(data2, function(d) { return d.close1; })]);
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    //y.domain([0, d3.max(data, function(d) { return d.close1; })]);
+    y.domain([0, d3.max(data, function(d) { return Math.max(d.close,d.close1); })]);
 //
-    var svg = d3.select("svg").transition();
-    svg.selectAll(".line")   // change the line
+    var _svg = d3.select("svg").transition();
+    /** Esto es parte de lo antiguo, funciona*/
+    _svg.selectAll(".line")   // change the line
         .duration(750)
-        .attr("d", valueline(data2))
+        .attr("d", valueline(data))
         ;
-    svg.select(".line2")   // change the line
+    _svg.select(".line2")   // change the line
         .duration(750)
         .attr("d", valueline2(data));
-    svg.select(".x.axis") // change the x axis
+    /** Aquí empieza lo nuevo*/
+    //var lines = svg.selectAll(".line").data(data2).attr("class","line");
+    //
+    //lines.transition()//.duration(750)
+    //    .attr("d",valueline(data2))
+    //    //.style("stroke", function(){
+    //    //    return '#'+Math.floor(Math.random()*16777215).toString(16);
+    //    //})
+    //    ;
+    //
+    //// enter any new data
+    //lines.enter()
+    //    .append("path")
+    //    //.attr("class","line")
+    //    .attr("d",valueline(data2))
+    //    .style("stroke", function(){
+    //        return '#'+Math.floor(Math.random()*16777215).toString(16);
+    //    });
+    //
+    //// exit
+    //lines.exit()
+    //    .remove();
+
+    /** Esto es parte de lo nuevo y antiguo*/
+    _svg.select(".x.axis") // change the x axis
         .duration(750)
         .call(xAxis);
         //.attr("transform", "translate(" + 0 + ",0)");
-    svg.select(".y.axis") // change the y axis
+    _svg.select(".y.axis") // change the y axis
         .duration(750)
         .call(yAxis);
         //.attr("transform", "translate(" + 0 + ",10)");
-    //changeData();
+    changeData();
 }
-//function changeData(){
+function changeData() {
 //    data2 = [
 //            {date: "1-May-12", close: 58.13, close1: 25.58},
 //            {date: "30-Apr-12", close: 45.98, close1: 57.58},
@@ -158,4 +195,38 @@ function removeChart(){
 //            {date: "24-Apr-12", close: 65.48, close1: 88.58}
 //        ]
 //        ;
-//}
+    if (_data == "data") {
+        _data = "data2";
+        data = [
+            {date: "1-May-12", close: 48.13, close1: 25.58},
+            {date: "30-Apr-12", close: 43.98, close1: 47.58},
+            {date: "27-Apr-12", close: 57.00, close1: 61.58},
+            {date: "26-Apr-12", close: 79.70, close1: 85.58},
+            {date: "25-Apr-12", close: 89.00, close1: 93.58},
+            {date: "24-Apr-12", close: 83.48, close1: 106.58}
+        ]
+        ;
+    } else if (_data == "data2") {
+        _data = "data3";
+        data = [
+            {date: "1-May-12", close: 28.13, close1: 39.58},
+            {date: "30-Apr-12", close: 33.98, close1: 47.58},
+            {date: "27-Apr-12", close: 37.00, close1: 53.58},
+            {date: "26-Apr-12", close: 45.70, close1: 57.58},
+            {date: "25-Apr-12", close: 49.00, close1: 60.58},
+            {date: "24-Apr-12", close: 53.48, close1: 68.58}
+        ]
+        ;
+    } else if (_data == "data3") {
+        _data = "data";
+        data = [
+            {date: "1-May-12", close: 58.13, close1: 45.58},
+            {date: "30-Apr-12", close: 53.98, close1: 47.58},
+            {date: "27-Apr-12", close: 67.00, close1: 51.58},
+            {date: "26-Apr-12", close: 89.70, close1: 65.58},
+            {date: "25-Apr-12", close: 99.00, close1: 73.58},
+            {date: "24-Apr-12", close: 130.48, close1: 96.58}
+        ]
+        ;
+    }
+}
